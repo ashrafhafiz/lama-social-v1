ffe21dc5335254323a561d44078b631abd33748d
Ashraf Hafiz <ashraf.hafiz@yahoo.com>
2023-10-18 20:45:42 +0300

    4th commit 18102023 - 08:55PM

---
+++ src/components/RightBar/RightBar.jsx
@@ -0,0 +1,143 @@
+import "./RightBar.scss";
+
+const RightBar = () => {
+  return (
+    <div className="rightbar">
+      <div className="container">
+        <div className="item">
+          <span>Suggestions for you</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=2048x2048&w=is&k=20&c=6omJSQtYXI93gG8zTc2aCJOCJrqBf-Bk8b_W-IXvy9s="
+                alt="Jane Doe"
+              />
+              <span>Jane Doe</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=2048x2048&w=is&k=20&c=6omJSQtYXI93gG8zTc2aCJOCJrqBf-Bk8b_W-IXvy9s="
+                alt="Jane Doe"
+              />
+              <span>Jane Doe</span>
+            </div>
+            <div className="buttons">
+              <button>Follow</button>
+              <button>Dismiss</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="item">
+          <span>Latest Activities</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=2048x2048&w=is&k=20&c=6omJSQtYXI93gG8zTc2aCJOCJrqBf-Bk8b_W-IXvy9s="
+                alt="Jane Doe"
+              />
+              <p>
+                <span>Jane Doe</span> Changed cover picture
+              </p>
+            </div>
+            <span>1 day ago</span>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=2048x2048&w=is&k=20&c=6omJSQtYXI93gG8zTc2aCJOCJrqBf-Bk8b_W-IXvy9s="
+                alt="Jane Doe"
+              />
+              <p>
+                <span>Jane Doe</span> Changed cover picture
+              </p>
+            </div>
+            <span>1 day ago</span>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=2048x2048&w=is&k=20&c=6omJSQtYXI93gG8zTc2aCJOCJrqBf-Bk8b_W-IXvy9s="
+                alt="Jane Doe"
+              />
+              <p>
+                <span>Jane Doe</span> Changed cover picture
+              </p>
+            </div>
+            <span>1 day ago</span>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=2048x2048&w=is&k=20&c=6omJSQtYXI93gG8zTc2aCJOCJrqBf-Bk8b_W-IXvy9s="
+                alt="Jane Doe"
+              />
+              <p>
+                <span>Jane Doe</span> Changed cover picture
+              </p>
+            </div>
+            <span>1 day ago</span>
+          </div>
+        </div>
+
+        <div className="item">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=2048x2048&w=is&k=20&c=6omJSQtYXI93gG8zTc2aCJOCJrqBf-Bk8b_W-IXvy9s="
+                alt="Jane Doe"
+              />
+              <div className="online"></div>
+              <span>Jane Doe</span>
+            </div>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=2048x2048&w=is&k=20&c=6omJSQtYXI93gG8zTc2aCJOCJrqBf-Bk8b_W-IXvy9s="
+                alt="Jane Doe"
+              />
+              <div className="online"></div>
+              <span>Jane Doe</span>
+            </div>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=2048x2048&w=is&k=20&c=6omJSQtYXI93gG8zTc2aCJOCJrqBf-Bk8b_W-IXvy9s="
+                alt="Jane Doe"
+              />
+              <div className="online"></div>
+              <span>Jane Doe</span>
+            </div>
+          </div>
+
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://media.istockphoto.com/id/1416048929/photo/woman-working-on-laptop-online-checking-emails-and-planning-on-the-internet-while-sitting-in.jpg?s=2048x2048&w=is&k=20&c=6omJSQtYXI93gG8zTc2aCJOCJrqBf-Bk8b_W-IXvy9s="
+                alt="Jane Doe"
+              />
+              <div className="online"></div>
+              <span>Jane Doe</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RightBar;
---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,29 @@
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext();
+
+export const AuthContextProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || false
+  );
+
+  const login = () => {
+    //  TODO
+    setCurrentUser({
+      id: "1",
+      name: "A. Hafiz",
+      profilePic:
+        "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    });
+  };
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(currentUser));
+  }, [currentUser]);
+
+  return (
+    <AuthContext.Provider value={{ currentUser, setCurrentUser, login }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
